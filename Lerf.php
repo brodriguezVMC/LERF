a4f8cb20c779971e271a749805cf84df73697a7c
Bruce Rodriguez <brodriguez@vmcsubastas.com>
2021-04-13 11:51:33 -0500

    lerf ok

--- Lerf.php
+++ Lerf.php
@@ -4,11 +4,33 @@ namespace Subascorp\Harmony\Math;
 
 class Lerf
 {
+    const CONSIGNMENT_BASE = 10;
     const PHI_1 = 11;
     const PHI_2 = 12;
     const PHI_3 = 13;
     const PHI_4 = 14;
     const PHI_5 = 15;
+    const VALUES = [
+        '-6' => [self::PHI_1, self::PHI_1],
+        '-5' => [self::PHI_1, self::PHI_2],
+        '-4' => [self::PHI_1, self::PHI_3],
+        '-3' => [self::PHI_1],
+        '-2' => [self::PHI_2],
+        '-1' => [self::PHI_3],
+        '0' => [],
+        '1' => [self::PHI_1, self::PHI_3],
+        '2' => [self::PHI_1, self::PHI_4],
+        '3' => [self::PHI_1],
+        '4' => [self::PHI_1, self::PHI_4],
+        '5' => [self::PHI_1, self::PHI_3],
+        '6' => [self::PHI_2, self::PHI_5],
+        '7' => [self::PHI_3],
+        '8' => [self::PHI_4],
+        '9' => [self::PHI_5],
+        '10' => []
+    ];
+    const ONLY_USD = [9];
+    const ONLY_SUB = [4, 5, 6];
 
     public static function value()
     {
@@ -20,106 +42,36 @@ class Lerf
         return $expectation/pow(self::value(), $iterations);
     }
 
-    public static function consignmentCalculation($userLevel, $basePrice)
+    public function consignmentCalculation($userLevel, $basePrice)
     {
-        $consignmentVMC = 0;
-        $consignmentUSD = 0;
-
-        if($userLevel == -6){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $phi1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $consignmentVMC = $consignmentBase + $phi1 + $phi1;
-
-        }elseif($userLevel == -5){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph2 = ceil(self::decompose($basePrice, self::PHI_2));
-            $consignmentVMC = $consignmentBase + $ph1 + $ph2;
-
-        }elseif($userLevel == -4){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph3 = ceil(self::decompose($basePrice, self::PHI_3));
-            $consignmentVMC = $consignmentBase + $ph1 + $ph3;
-            
-        }elseif($userLevel == -3){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $consignmentVMC = $consignmentBase + $ph1;
-
-        }elseif($userLevel == -2){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $ph2 = ceil(self::decompose($basePrice, self::PHI_2));
-            $consignmentVMC = $consignmentBase + $ph2;
-
-        }elseif($userLevel == -1){
-            $consignmentBase = ceil(self::decompose($basePrice, 10));
-            $ph3 = ceil(self::decompose($basePrice, self::PHI_3));
-            $consignmentVMC = $consignmentBase + $ph3;
-
-        }elseif($userLevel == 0){
-            $consignment = ceil(self::decompose($basePrice, 10));
-            $consignmentVMC = $consignment;
-            $consignmentUSD = $consignmentVMC * 2;
-            
-        }elseif($userLevel == 1){
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph3 = ceil(self::decompose($basePrice, self::PHI_3));
-            $consignmentVMC = $ph1 + $ph3;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 2){
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph4 = ceil(self::decompose($basePrice, self::PHI_4));
-            $consignmentVMC = $ph1 + $ph4;
-            $consignmentUSD = $consignmentVMC * 2;
-            
-        }elseif($userLevel == 3){
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $consignmentVMC = $ph1;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 4){
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph4 = ceil(self::decompose($basePrice, self::PHI_4));
-            $consignmentVMC = $ph1 - $ph4;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 5){
-            $ph1 = ceil(self::decompose($basePrice, self::PHI_1));
-            $ph3 = ceil(self::decompose($basePrice, self::PHI_3));
-            $consignmentVMC = $ph1 - $ph3;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 6){
-            $ph2 = ceil(self::decompose($basePrice, self::PHI_2));
-            $ph5 = ceil(self::decompose($basePrice, self::PHI_5));
-            $consignmentVMC = $ph2 - $ph5;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 7){
-            $ph3 = ceil(self::decompose($basePrice, self::PHI_3));
-            $consignmentVMC = $ph3;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 8){
-            $ph4 = ceil(self::decompose($basePrice, self::PHI_4));
-            $consignmentVMC = $ph4;
-            $consignmentUSD = $consignmentVMC * 2;
-
-        }elseif($userLevel == 9){
-            $ph5 = ceil(self::decompose($basePrice, self::PHI_5));
-            $consignmentUSD = $ph5 * 2;
-
-        }elseif($userLevel == 10){
-            $consignmentVMC = 0;
-            $consignmentUSD = 0;
-        }
-
+        $consignmentBase = $userLevel <= 0 ? ceil(self::decompose($basePrice, self::CONSIGNMENT_BASE)) : null;
+        
+        $calculatePhi = $this->calculatePhi($basePrice, $userLevel);
+        $consignmentVMC =  $consignmentBase + $calculatePhi;
+        $consignmentUSD = $this->consignmentUSD($consignmentVMC, $userLevel);
         return [
-            'VMC' => $consignmentVMC,
-            'USD' => $consignmentUSD
+            'subascoin' => !in_array(9, self::ONLY_USD) ? $consignmentVMC : null,
+            'saldo' => $consignmentUSD
         ];
     }
+
+    public function calculatePhi(int $basePrice, String $userLevel): int
+    {
+        $result = 0;
+        foreach (self::VALUES[$userLevel] as $key => $value) {
+            if (in_array($userLevel, self::ONLY_SUB) and $key > 0) {
+                $result -= ceil(self::decompose($basePrice, $value));
+            } else {
+                $result += ceil(self::decompose($basePrice, $value));
+            }
+        }
+        return $result;
+    }
+
+    public function consignmentUSD(int $amount, int $userLevel):? int
+    {
+        return $userLevel >= 0 ? $amount * 2 : null;
+    }
 }
-print_r((new Lerf())->consignmentCalculation(10, 12000));
+
+print_r((new Lerf())->consignmentCalculation(9, 12000));
